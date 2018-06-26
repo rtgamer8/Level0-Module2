dc7d619ffe6f9f398bb898b5571f87fdc762c9a7
DaveDLeague <dave.dunn@jointheleague.org>
2018-06-26 14:01:09 -0700

    Update Validation.java

--- src/random/Validation.java
+++ src/random/Validation.java
@@ -9,21 +9,17 @@ import javax.swing.JOptionPane;
 
 public class Validation {
 	public static void main(String[] args) {
-
-		// 1. OPTIONAL: Watch the first 2 minutes of this movie: http://vimeo.com/2485018
 		
 		Random randomMaker = new Random();
 		
 		int randomNumber = randomMaker.nextInt(5);
 		
-		// 2. On paper, write all the numbers that get printed when you run this class 10 times
-		
 		System.out.println(randomNumber);
 
-		// 3. Use each value of randomNumber to give the user a random compliment.
+		// 1. Use each value of randomNumber to give the user a random compliment.
 
-		// 4. Repeat all the code above 10 times
+		// 2. Repeat all the code above 10 times
 		
-		// 5. Find someone to test out your program. They will like it :)
+		// 3. Find someone to test out your program. They will like it :)
 	}
 }
