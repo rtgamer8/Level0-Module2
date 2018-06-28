daae46e4fe59dbcedde4958fafee7d1ff59ab3be
league <league@worthymini-5.attlocal.net>
2018-06-28 15:35:52 -0700

    Fixed playSound not working on Linux

--- src/methods/AnimalFarm.java
+++ src/methods/AnimalFarm.java
@@ -1,62 +1,63 @@
 package methods;
 
 //Copyright (c) The League of Amazing Programmers 2013-2017
-//Level 0
 
+//Level 0
 
 import java.io.File;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
 import javax.swing.JOptionPane;
 
 public class AnimalFarm {
 
-AnimalFarm() {
-	/*
-	 * 1. Ask the user which animal they want, then play the sound of that
-	 * animal.
-	 */
-
-	/* 2. Make it so that the user can keep entering new animals. */
-}
-
-void playMoo() {
-	playNoise(mooFile);
-}
-
-void playQuack() {
-	playNoise(quackFile);
-}
-
-void playWoof() {
-	playNoise(woofFile);
-}
-
-String quackFile = "quack.wav";
-String mooFile = "moo.wav";
-String woofFile = "woof.wav";
-String meowFile = "meow.wav";
-String llamaFile = "llama.wav";
-
-
-/* Ignore this stuff */
-
-public void playNoise(String soundFile) {
-	try {
-		 Clip clip = AudioSystem.getClip();
-		 clip.open(AudioSystem.getAudioInputStream(getClass().getResource(soundFile)));
-		 clip.start();
-		 Thread.sleep(3400);
-	} catch (Exception ex) {
-  	ex.printStackTrace();
+	AnimalFarm() {
+		/*
+		 * 1. Ask the user which animal they want, then play the sound of that animal.
+		 */
+		
+		/* 2. Make it so that the user can keep entering new animals. */
 	}
-}
 
-public static void main(String[] args) {
-	new AnimalFarm();
-}
+	void playMoo() {
+		playNoise(mooFile);
+	}
+
+	void playQuack() {
+		playNoise(quackFile);
+	}
+
+	void playWoof() {
+		playNoise(woofFile);
+	}
+
+	String quackFile = "quack.wav";
+	String mooFile = "moo.wav";
+	String woofFile = "woof.wav";
+	String meowFile = "meow.wav";
+	String llamaFile = "llama.wav";
+
+	/* Ignore this stuff */
+
+	public void playNoise(String soundFile) {
+		Clip clip = null;
+		try {
+			AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File("src/methods/" + soundFile));
+			DataLine.Info info = new DataLine.Info(Clip.class, inputStream.getFormat());
+			clip = (Clip) AudioSystem.getLine(info);
+			clip.open(inputStream);
+			clip.start();
+			Thread.sleep(3400);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		new AnimalFarm();
+	}
 
 }
-
